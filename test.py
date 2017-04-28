41019880e057e7b8b56a4a8cc4f4916511415d12
clarke07 <clarke@wiivv.com>
2017-04-28 16:20:04 -0700

    change

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import app
 
 class MyTests(unittest.TestCase):
 	def test(self):
-		self.assertEqual(app.lambda_handler('',''), 'Hello from Lambda 12345')
+		self.assertEqual(app.lambda_handler('',''), 'Hello from Lambda 12345666666')
 
 if __name__ == '__main__':
 	unittest.main()
