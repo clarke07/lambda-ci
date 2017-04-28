9e1dc63238b8fb7919fa6fd66e260b9a5e4cb28d
clarke07 <clarke@wiivv.com>
2017-04-28 15:36:16 -0700

    add test

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+import app
+
+class MyTests(unittest.TestCase):
+	def test(self):
+		self.assertEqual(app.lambda_handler('',''), 'Hello from Lambda 12345')
+
+if __name__ == '__main__':
+	unittest.main()
